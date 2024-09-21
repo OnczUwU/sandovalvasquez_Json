72ddd36b568fc78c41c7a3587d3e4912a2f168c6
OnczUwU <140672241+OnczUwU@users.noreply.github.com>
2024-09-21 03:33:59 +0200

    Se reparo un error
    se hizo la corrección de un signo que estaba mal colocado

--- conn.php
+++ conn.php
@@ -2,36 +2,24 @@
 
 
 class conexion{
-
-public $host = '127.0.0.1';
-public $db = 'rrhh';
-public $user = 'root';
-public $pass = '';
-public $port = '3306';
-public $charset = 'utf8mb4';
-public $options = [
+    public $host = '127.0.0.1';
+    public $db = 'rrhh';
+    public $user = 'root';
+    public $pass = '';
+    public $port = '3306';
+    public $charset = 'utf8mb4';
+    public $options = [
     \PDO:: ATTR_ERRMODE=> \PDO::ERRMODE_EXCEPTION,
     \PDO:: ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
     \PDO:: ATTR_EMULATE_PREPARES => false
 ]; 
-
-public function concetar(){
-    try{
-        $pdo = new PDO("mysql:host0{$this->host};dbname={$this->db};charset={$this->charset};port{$this->port}",$this->user,$this->pass,$this->options);
-        return $pdo;
-
-    }catch(PDOException $exp){
-        echo("HUBO UN ERROR EN LA CONEXION");
+    public function concetar(){
+        try{
+            $pdo = new PDO("mysql:host={$this->host};dbname={$this->db};charset={$this->charset};port{$this->port}",$this->user,$this->pass,$this->options);
+            return $pdo;
+        }catch(PDOException $exp){
+            echo("HUBO UN ERROR EN LA CONEXION".$exp->getMessage());
+        }
     }
-} 
-
 }
-
-
-
-
-
-
-
-
 ?>
